8eb1d9a81cb6c025fea7e705113b2813460ca60d
Viet Dinh <54ckb0y789@gmail.com>
2023-08-21 00:41:26 -0400

    feat: catch panics (todo)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::any::Any;
 use std::borrow::Cow;
 use std::cmp::Ordering;
+use std::panic::AssertUnwindSafe;
 use std::path::Path;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use dashmap::DashMap;
+use futures::future::CatchUnwind;
+use futures::{Future, FutureExt};
 use miette::{diagnostic, IntoDiagnostic};
 use ropey::Rope;
 use serde_json::Value;
+use tower::{Service, ServiceBuilder};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::notification::Progress;
 use tower_lsp::lsp_types::request::WorkDoneProgressCreate;
@@ -663,5 +670,76 @@ async fn main() {
 	})
 	.finish();
 
+	// let layer = tower::layer::layer_fn(|service| {
+	// 	struct CatchPanic<S>(S);
+	// 	CatchPanic(service)
+	// });
+
+	// let service = ServiceBuilder::new().layer_fn(CatchPanic).service(service);
+
 	Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+// copied from tower_http::catch_panic
+struct CatchPanic<S>(S);
+impl<S> Service<tower_lsp::jsonrpc::Request> for CatchPanic<S>
+where
+	S: Service<tower_lsp::jsonrpc::Request, Response = Option<tower_lsp::jsonrpc::Response>>,
+{
+	type Response = S::Response;
+	type Error = S::Error;
+	type Future = CatchPanicFuture<S::Future>;
+
+	#[inline]
+	fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<std::result::Result<(), Self::Error>> {
+		self.0.poll_ready(cx)
+	}
+
+	fn call(&mut self, req: tower_lsp::jsonrpc::Request) -> Self::Future {
+		match std::panic::catch_unwind(AssertUnwindSafe(|| self.0.call(req))) {
+			Ok(fut) => CatchPanicFuture {
+				kind: Kind::Future {
+					future: AssertUnwindSafe(fut).catch_unwind(),
+				},
+			},
+			Err(panic_err) => CatchPanicFuture {
+				kind: Kind::Panicked { panic_err },
+			},
+		}
+	}
+}
+
+pin_project_lite::pin_project! {
+	struct CatchPanicFuture<S> {
+		#[pin]
+		kind: Kind<S>
+	}
+}
+pin_project_lite::pin_project! {
+	#[project = KindProj]
+	enum Kind<S> {
+		Panicked {
+			panic_err: Box<dyn Any + Send + 'static>
+		},
+		Future {
+			#[pin]
+			future: CatchUnwind<AssertUnwindSafe<S>>
+		}
+	}
+}
+
+impl<S, R, E> Future for CatchPanicFuture<S>
+where
+	S: Future<Output = core::result::Result<R, E>>,
+{
+	type Output = core::result::Result<Option<tower_lsp::jsonrpc::Response>, E>;
+
+	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+		todo!()
+		// match self.project().kind.project() {
+		// 	KindProj::Panicked { panic_err } => Poll::Ready(Ok(Some({
+
+		// 	}))),
+		// }
+	}
 }
