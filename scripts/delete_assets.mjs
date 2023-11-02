bc4e6f1edaceb52cd06f04d27a1b868b7a849cbd
Viet Dinh <54ckb0y789@gmail.com>
2023-11-01 22:33:06 -0400

    chore: script to delete old release assets

---
+++ scripts/delete_assets.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env zx
+import { Octokit } from '@octokit/rest'
+
+const cutOff = 10;
+const owner = 'Desdaemon';
+const repo = 'odoo-lsp';
+let auth = await $`gh auth token`;
+auth = auth.stdout.trim();
+
+const gh = new Octokit({ request: { fetch }, auth });
+const releases = await gh.repos.listReleases({ owner, repo });
+const oldReleases = releases.data
+  .filter(rel => rel.prerelease && rel.tag_name.startsWith('nightly'))
+  .sort((a, z) => z.created_at.localeCompare(a.created_at))
+  .slice(cutOff);
+for (const rel of oldReleases) {
+  console.log(rel.name);
+  for (const asset of rel.assets) {
+    console.log('.. delete ', asset.name);
+    await gh.repos.deleteReleaseAsset({ owner, repo, asset_id: asset.id });
+  }
+}
