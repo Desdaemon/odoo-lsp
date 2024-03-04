9fc87c86b36b1977efad5afe53946773e95477fb
Viet Dinh <54ckb0y789@gmail.com>
2024-03-04 10:37:06 -0500

    chore: update delete assets script

--- scripts/delete_assets.mjs
+++ scripts/delete_assets.mjs
@@ -14,7 +14,7 @@ const oldReleases = releases.data
   .sort((a, z) => z.created_at.localeCompare(a.created_at))
   .slice(cutOff);
 for (const rel of oldReleases) {
-  console.log(rel.name);
+  console.log(rel.tag_name);
   for (const asset of rel.assets) {
     console.log('.. delete ', asset.name);
     await gh.repos.deleteReleaseAsset({ owner, repo, asset_id: asset.id });
